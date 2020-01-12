470e28134d1ac95ec1a462f3db63a22a0b51833b
Louis Hough <sirlouishough@gmail.com>
2020-01-12 23:09:37 +0000

    function to simulate one round taking the human choice and comparing to simulated computer choice - result stored in variable

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-function computerPlay() {
+let result;
+let humanChoice;
 
+function computerPlay() {
     randomNum = Math.floor(Math.random() * 3) + 1;  
 
     if (randomNum == 1) {
         return "rock";
     } else if (randomNum == 2) {
         return "paper";
-    } 
-        return "scissors";
+    }   return "scissors";
 }
 
+function playRound(humanChoice, computerChoice) {
+    computerChoice = computerPlay();
+    
+    if (humanChoice == computerChoice) result = "draw";
+
+    else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper"
+    && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
+        result = "Player Win";
+    } else result = "Computer Win";
+
+}
+
+
+
+
+(playRound("rock"));
+console.log(result);
